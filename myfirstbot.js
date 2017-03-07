eb199ff375160ce409b383b761040b1336c1bb7f
Deanna Sturgell <drobison@microsoft.com>
2017-03-07 14:53:10 -0800

    addrestify

--- myfirstbot.js
+++ myfirstbot.js
@@ -1,17 +1,25 @@
 var builder      = require('botbuilder');
-var connector    = new builder.ConsoleConnector().listen();   
+var restify      = require('restify');
+
+var connector    = new builder.ChatConnector();   
 var bot          = new builder.UniversalBot(connector);
 
 bot.dialog('/', [
     session => {builder.Prompts.text(session, "what is your name?") },
     (session,result) => {
-        if(result.response == 'deanna')
+        if(result.response == 'deanna' || result.response == 'kevin')
         {
-            session.send("cool name");
+            session.send("i like that name");
         }
         else
         {
-            session.send("that name sucks");
+            session.send("that's nice");
         }
     }
 ])
+
+var server = restify.createServer();
+server.listen(3978, function() {
+    console.log('test bot endpoint at http://localhost:3978/api/messages');
+});
+server.post('/api/messages', connector.listen());
